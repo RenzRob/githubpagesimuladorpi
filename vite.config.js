2014713cc87e3b50e259412ada26999d05c2a35d
renzorobles <renzomartinrobles99@gmail.com>
2025-05-26 20:58:07 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// filepath: /Users/renzorobles/Desktop/montecarlo-pi-react/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/montecarlo-pi-react/', // <-- add this line
 })
