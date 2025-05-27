ddd6b4157852791ab701c00ab2f845cd3feb08d3
renzorobles <renzomartinrobles99@gmail.com>
2025-05-26 22:12:56 -0300

    gh-pages añadido

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// filepath: /Users/renzorobles/Desktop/montecarlo-pi-react/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/montecarlo-pi-react/', // <-- add this line
+  base: "/githubpagesimuladorpi/"
 })
